b7284966e91154692ad8c7af9ef74131286cb11a
priyanshu1019 <goutampriyanshu27@gmail.com>
2024-07-03 17:00:16 +0530

    Designed Login-UI

--- app/src/main/java/com/example/echoplex/SignupActivity.kt
+++ app/src/main/java/com/example/echoplex/SignupActivity.kt
@@ -1,31 +1,20 @@
 package com.example.echoplex
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.echoplex.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
-    private val binding: ActivityLoginBinding by lazy{
-        ActivityLoginBinding.inflate(layoutInflater)
-    }
+class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(binding.main)
+        setContentView(R.layout.activity_signup)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val tvSignup = binding.tvSignup
-        tvSignup.setOnClickListener{
-            val intent: Intent = Intent(this , SignupActivity::class.java)
-            startActivity(intent)
         }
     }
 }
