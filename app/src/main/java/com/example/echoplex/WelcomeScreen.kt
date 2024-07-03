d39436062c3a289f13c9e297ad6fda83be5531b6
priyanshu1019 <goutampriyanshu27@gmail.com>
2024-07-03 15:33:24 +0530

    Completed Welcome Screen

---
+++ app/src/main/java/com/example/echoplex/WelcomeScreen.kt
@@ -0,0 +1,35 @@
+package com.example.echoplex
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.echoplex.databinding.ActivityWelcomeScreenBinding
+
+class WelcomeScreen : AppCompatActivity() {
+    private val binding: ActivityWelcomeScreenBinding by lazy{
+        ActivityWelcomeScreenBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //move to login screen
+        val getStarted = binding.getStarted
+        getStarted.setOnClickListener{
+            val intent:Intent = Intent(this , LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
