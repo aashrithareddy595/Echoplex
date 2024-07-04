07d6aac4e18be82b80dc8277f70179a281a3d356
priyanshu1019 <goutampriyanshu27@gmail.com>
2024-07-04 08:46:32 +0530

    Completed firebase-auth login and signup

--- app/src/main/java/com/example/echoplex/LoginActivity.kt
+++ app/src/main/java/com/example/echoplex/LoginActivity.kt
@@ -3,20 +3,36 @@ package com.example.echoplex
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.echoplex.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
     private val binding: ActivityLoginBinding by lazy{
         ActivityLoginBinding.inflate(layoutInflater)
     }
+    private lateinit var auth: FirebaseAuth
+    override fun onStart() {
+        super.onStart()
+        //check if user already logged-in
+        val currentUser :FirebaseUser? = auth.currentUser
+        if( currentUser!= null){
+            startActivity(Intent(this , MainActivity::class.java))
+            finish()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.main)
+        auth = FirebaseAuth.getInstance()
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -24,9 +40,28 @@ class LoginActivity : AppCompatActivity() {
         }
         val tvSignup = binding.tvSignup
         tvSignup.setOnClickListener{
-            val intent: Intent = Intent(this , SignupActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this , SignupActivity::class.java))
             finish()
+        }
+
+        binding.loginBtn.setOnClickListener{
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            if( email.isEmpty()||password.isEmpty()){
+                Toast.makeText(this , "Please fill all the details!" , Toast.LENGTH_SHORT).show()
+            }else{
+                auth.signInWithEmailAndPassword(email , password)
+                    .addOnCompleteListener{task->
+                        if(task.isSuccessful){
+                            Toast.makeText(this , "Log-In successful!" , Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(this , MainActivity::class.java))
+                            finish()
+                        }else{
+                            Toast.makeText(this , "${task.exception?.message}" , Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
         }
     }
 }
--- app/src/main/java/com/example/echoplex/SignupActivity.kt
+++ app/src/main/java/com/example/echoplex/SignupActivity.kt
@@ -2,31 +2,61 @@ package com.example.echoplex
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.echoplex.databinding.ActivityLoginBinding
 import com.example.echoplex.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignupActivity : AppCompatActivity() {
     private val binding : ActivitySignupBinding by lazy {
         ActivitySignupBinding.inflate(layoutInflater)
     }
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.signuplayout)
+        //initialize firebase auth
+        auth = FirebaseAuth.getInstance()
         ViewCompat.setOnApplyWindowInsetsListener(binding.signuplayout) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
         val tvLogin = binding.tvLogin
         tvLogin.setOnClickListener{
-            val intent : Intent = Intent(this , LoginActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this , LoginActivity::class.java))
             finish()
+        }
+        binding.submitBtn.setOnClickListener{
+            val email = binding.etEmail.text.toString()
+            val name = binding.etName.text.toString()
+            val password = binding.etPassword.text.toString()
+            val password2 = binding.etPassword2.text.toString()
+
+            if(email.isEmpty()||name.isEmpty()||password.isEmpty()||password2.isEmpty()){
+                Toast.makeText(this , "Please fill all the details!" , Toast.LENGTH_SHORT).show()
+            }else if( password!=password2){
+                Toast.makeText(this , "Passwords do not match!" , Toast.LENGTH_SHORT).show()
+            }else{
+                auth.createUserWithEmailAndPassword(email , password)
+                    .addOnCompleteListener(this){task->
+                        if(task.isSuccessful){
+                            Toast.makeText(this , "Sign up successful!" , Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(this , LoginActivity::class.java))
+                            finish()
+                        }else{
+                            Toast.makeText(this , "${task.exception?.message}" , Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+
         }
     }
 }
