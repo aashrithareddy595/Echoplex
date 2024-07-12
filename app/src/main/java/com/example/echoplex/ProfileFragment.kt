62c8a8c95f2863f79ba0d5fb10b4fd683b1f0c83
priyanshu1019 <goutampriyanshu27@gmail.com>
2024-07-12 09:28:29 +0530

    added edit profile section in profile fragment

--- app/src/main/java/com/example/echoplex/ProfileFragment.kt
+++ app/src/main/java/com/example/echoplex/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.echoplex
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,13 +9,16 @@ import android.view.ViewGroup
 
 class ProfileFragment : Fragment() {
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
-    }
+        // Directly start ProfileActivity
+        startActivity(Intent(activity, ProfileActivity::class.java))
+        // Finish the current fragment to avoid showing it
+        activity?.supportFragmentManager?.beginTransaction()?.remove(this)?.commit()
 
+        // Return null since the fragment won't be displayed
+        return null
+    }
 }
